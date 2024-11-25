9ba735a770e5f8537560d9197680c3d539590a0c
Shubhajeet123 <102015280+Shubhajeet123@users.noreply.github.com>
2024-11-25 18:05:11 +0530

    Add files via upload

---
+++ Day-4/main.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_bucket" "name" {
+    bucket = "myfamilybuckett"
+  
+}
+
+# resource "aws_s3_bucket" "name" {
+#     bucket = "myweekendbucketttttttttts"
+  
+# }
+
+terraform {
+  backend "s3" {
+    bucket = "mybuckettttsssss"
+    region = "ap-south-1"
+    key = "terraform.tfstate"
+    encrypt = true
+    
+  }
+}
